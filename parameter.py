6b19930032564cdc8119df822328cea39be64993
Matteo Caligiuri <matteo.caligiuri@dei.unipd.it>
2025-08-11 19:48:17 +0200

    Removed all suggestion to parameters file

--- parameter.py
+++ parameter.py
@@ -8,15 +8,15 @@ from . import const
 
 
 # atmosphere
-const.VISIBILITY_RANGE_MOLECULE = 12  # m    12
-const.VISIBILITY_RANGE_AEROSOL = 450  # m     450
+const.VISIBILITY_RANGE_MOLECULE = 12  # m
+const.VISIBILITY_RANGE_AEROSOL = 450  # m
 const.ECM = 3.912 / const.VISIBILITY_RANGE_MOLECULE  # EXTINCTION_COEFFICIENT_MOLECULE /m
 const.ECA = 3.912 / const.VISIBILITY_RANGE_AEROSOL  # EXTINCTION_COEFFICIENT_AEROSOL /m
 
-const.FT = 31  # FOG_TOP m  31  70
-const.HT = 300  # HAZE_TOP m  300    34
+const.FT = 31  # FOG_TOP m
+const.HT = 300  # HAZE_TOP m
 
 # camera
-const.CAMERA_ALTITUDE = 50  #  m fog 50   1.8
+const.CAMERA_ALTITUDE = 50  #  m
 const.HORIZONTAL_ANGLE = 0  #  °
 const.CAMERA_VERTICAL_FOV = 64  #  °
